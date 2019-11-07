0c254626f8ed6cfedba00146322a94f33d74c8a9
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2019-11-07 15:12:09 +0100

    added minilib MBH and set-up meson environment for compiling

---
+++ mbh/include/mbh.h
@@ -0,0 +1,30 @@
+#pragma once
+
+/// A monster bitch holder engine
+struct MBH {
+
+  /**
+   * @name Ctor/Dtor
+   *
+   * @{ 
+   */
+  
+  /// build your engine
+  MBH ();
+
+  ~MBH () = default;
+
+  ///@} End of Ctor/Dtor
+
+  /**
+   * @name Public functions of the class
+   *
+   * @{ 
+   */
+
+  /// Make your monster hold that bitch
+  void hold_that_bitch ();
+
+  ///@} End of public functions of the class
+
+}; // endstruct MBH
---
+++ mbh/src/mbh.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <mbh.h>
+
+MBH::MBH () {
+
+  std::cout << "I'm ready to hold.\n";
+
+}
+
+void MBH::hold_that_bitch () {
+
+  std::cout << "I'm trying man!\n";
+  return;
+
+}
